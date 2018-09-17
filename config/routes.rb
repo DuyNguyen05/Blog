e26e903e35142d97e23c270cfc7427c467d5875c
Duy Nguyen <nguyenngocduy.84@live.com>
2018-09-17 21:10:01 +0700

    add Profile

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :posts
   resources :stories
   resources :lifes
+  resources :profiles
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_12_060220) do
+ActiveRecord::Schema.define(version: 2018_09_17_062343) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.date "birthday"
+    t.string "university"
+    t.string "skills"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
